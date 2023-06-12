0bb99949558bde4be648d393bade7f0f2b33e54c
Holger Hinzberg <holger@hinzberg.de>
2023-06-12 19:56:51 +0200

    Initial Commit

---
+++ SwiftChartsDemo/SwiftChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsDemoApp.swift
+//  SwiftChartsDemo
+//
+//  Created by Holger Hinzberg on 12.06.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
