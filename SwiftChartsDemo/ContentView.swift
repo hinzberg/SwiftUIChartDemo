aab8970472c78fef43c6c682022914ac8d0a7aee
Holger Hinzberg <holger@hinzberg.de>
2023-07-03 21:42:14 +0200

    2023-07-03 Chart Axis Configuration

--- SwiftChartsDemo/ContentView.swift
+++ SwiftChartsDemo/ContentView.swift
@@ -22,21 +22,63 @@ struct ContentView: View {
     ];
     
     var body: some View {
-        Chart {
-            ForEach(costs) { cost in
-                BarMark(
-                    x: .value("Description", cost.description),
-                    y: .value("Cost", cost.value)
-                )
-                .cornerRadius(10)
-                .foregroundStyle(cost.color)
-                .annotation(position: .overlay) {
-                    Text("\(cost.value.formatted())")
-                        .font(.headline)
-                        .foregroundColor(.white)
+            Chart {
+                RuleMark( y: .value("Max", 90))
+                    .foregroundStyle(Color.red)
+                    .lineStyle(StrokeStyle(lineWidth: 2, dash: [5,8]))
+                    .annotation(alignment: .trailing) {
+                        Text("Max  ")
+                            .font(.headline)
+                            .foregroundColor(.red)
+                    }
+                    
+                ForEach(costs) { cost in
+                    BarMark(
+                        x: .value("Description", cost.description),
+                        y: .value("Cost", cost.value)
+                    )
+                    .cornerRadius(10)
+                    .foregroundStyle(cost.color.gradient)
+                    .annotation(position: .overlay) {
+                        Text("\(cost.value.formatted()) €")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
                 }
             }
-        }
+        
+            .chartXAxis {
+                AxisMarks() { value in
+                    // Gridlines
+                    AxisGridLine(centered: false, stroke: StrokeStyle(dash: [1, 10]))
+                        .foregroundStyle(.blue.gradient)
+                    // Tick Marks
+                    AxisTick(centered: false, stroke: StrokeStyle(dash: [1, 2]))
+                        .foregroundStyle(.red)
+                    // Value Labels
+                    AxisValueLabel() {
+                        if let description = value.as(String.self) {
+                            Text("\(description)")
+                                .foregroundStyle(.purple)
+                                .font(.title)
+                        }
+                    }
+                }
+            }
+        
+         //   .chartYScale(domain: 0...250)
+        /*
+            .chartXAxis(.hidden)
+            .chartYAxis(.hidden)
+        */
+        /*
+            // Plot Area
+            .chartPlotStyle{ plotContent in
+                plotContent
+                    .background(.blue.gradient.opacity(0.1))
+                    .border(.black, width: 1)
+            }
+         */
         .padding()
     }
 }
